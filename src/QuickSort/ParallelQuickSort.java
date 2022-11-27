e41f5f88b8516e36016d020623cb6356f95a9ca4
OrangeX4 <318483724@qq.com>
2022-11-27 23:51:48 +0800

    feat: complete basic MapReduce ParallelQuickSort

---
+++ src/QuickSort/ParallelQuickSort.java
@@ -0,0 +1,38 @@
+package QuickSort;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParallelQuickSort {
+    private static void swap(List<Integer> numbers, int i, int j) {
+        int temp = numbers.get(i);
+        numbers.set(i, numbers.get(j));
+        numbers.set(j, temp);
+    }
+
+    private static int partition(List<Integer> numbers, int left, int right) {
+        int pivot = numbers.get(right);
+        int i = left - 1;
+        for (int j = left; j < right; j++) {
+            if (numbers.get(j) <= pivot) {
+                i++;
+                swap(numbers, i, j);
+            }
+        }
+        swap(numbers, i + 1, right);
+        return i + 1;
+    }
+
+    private static void _quickSort(List<Integer> numbers, int left, int right) {
+        if (left < right) {
+            int pivot = partition(numbers, left, right);
+            _quickSort(numbers, left, pivot - 1);
+            _quickSort(numbers, pivot + 1, right);
+        }
+    }
+
+    public static List<Integer> quickSort(List<Integer> numbers) {
+        List<Integer> sortedNumbers = new ArrayList<>(numbers);
+        _quickSort(sortedNumbers, 0, sortedNumbers.size() - 1);
+        return sortedNumbers;
+    }
+}
